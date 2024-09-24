37ee0a601179cb831360631719816d2a63427e1c
khadijagilt <khadijaabkir03@gmail.com>
2024-09-24 18:57:36 +0200

    sas

---
+++ Day01/challenge1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+   char  nom[10], prenom[10], email[40], sexe[10];
+int age;
+printf("entey votre nom:");
+scanf("%s", name);
+printf("enter votre prenom:");
+scanf("%s", prenom);
+printf("entrey votre email:");
+scanf("%s"; email);
+printf("entrez votre sexe");
+scanf("%s", sexe);
+printf("entrez votre age:");
+scanf("%d", &age);
+printf("\n Informations Personnelles \n");
+ printf("Nom : %s\n", nom);
+printf("Penom : %s\n", prenom);
+ printf("age : %d\n", age);
+ printf("sexe : %s\n", sexe);
+ printf("adresse email : %s\n", email);
+}
+
+
---
+++ Day01/challenge2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+float C, K;
+printf("entrez la temperature en celsuis");
+scanf("%f", &C);
+float K = C + 273,15;
+printf("la temperature %f en kelvin est %.2f \n", C, K);
+
+
+
+
+
+}
